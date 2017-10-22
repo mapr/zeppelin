813d26e5cc2449232ed1f54b2bf2df56bf3a725c
Ivan Dzikovskyi <idzikovskyi@mapr.com>
2017-10-22 16:37:42 +0300

    MSPARK-99 SparkVersion file is not present in /opt/mapr/spark

--- scripts/mapr-pacc/src/preconfigure.sh
+++ scripts/mapr-pacc/src/preconfigure.sh
@@ -2,6 +2,23 @@
 
 MAPR_HOME=${MAPR_HOME:-/opt/mapr}
 
+
+get_spark_home() {
+    local SPARK_HOME=""
+    local SPARK_VERSION=""
+    local spark_home_legacy=""
+    if [ -e "${MAPR_HOME}/spark/sparkversion" ]; then
+        SPARK_VERSION=$(cat "${MAPR_HOME}/spark/sparkversion")
+        SPARK_HOME="${MAPR_HOME}/spark/spark-${SPARK_VERSION}"
+    else
+        # Legacy way to find SPARK_HOME
+        spark_home_legacy=$(find "${MAPR_HOME}/spark/" -maxdepth 1 -name "spark-*" -type d | tail -n1)
+        [ -e "${spark_home_legacy}" ] && SPARK_HOME="${spark_home_legacy}"
+    fi
+    echo "${SPARK_HOME}"
+}
+
+
 mkdir -p "${MAPR_HOME}/zeppelin/thirdparty/jdbc-mapr-drill"
 
 curl -o /tmp/DrillJDBC41.zip "http://package.mapr.com/tools/MapR-JDBC/MapR_Drill/MapRDrill_jdbc_v1.5.3.1006/DrillJDBC41.zip"
@@ -9,10 +26,8 @@ unzip -d "${MAPR_HOME}/zeppelin/thirdparty/jdbc-mapr-drill" /tmp/DrillJDBC41.zip
 rm /tmp/DrillJDBC41.zip
 
 
-if [ -e "${MAPR_HOME}/spark/sparkversion" ]; then
-    SPARK_VERSION=$(cat "${MAPR_HOME}/spark/sparkversion")
-    SPARK_HOME="${MAPR_HOME}/spark/spark-${SPARK_VERSION}"
-
+SPARK_HOME=$(get_spark_home)
+if [ -e "${SPARK_HOME}" ]; then
     # Copy MapR-DB and Streaming jars into Spark
     JAR_WHILDCARDS="
         ${MAPR_HOME}/lib/kafka-clients-*-mapr-*.jar
